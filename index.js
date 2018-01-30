07a5ef1f7a5e6739ba9f278c57839398fb44d874
Dori Levy <dori.levy@wework.com>
2018-01-30 18:56:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(players, instru) {
   var arr = [];
   var i = 0;
   for ( ; i < players.lenght ; i++ ) {
-    console.log(decodeURI)
+    console.log(i)
     arr.push(players[i] + " plays " + instru[i]);
   }
   return arr;
